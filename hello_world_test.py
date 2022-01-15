4939c739fcd455f889d7a9ae9b716c91f8875742
Park-Tae-Ju <32889481+Park-Tae-Ju@users.noreply.github.com>
2022-01-15 17:11:18 +0900

    Add files via upload

--- hello_world_test.py
+++ hello_world_test.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.6
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+#파이썬(.py)을 생성하는 방법
+# -
+
+# !jupyter notebook --generate-config
+
 print('hello world')
 
 
